26e175501030d4377e22fb95707f1d63d2302fa9
MW <wieczorek0807@gmail.com>
2024-11-27 14:34:17 +0100

    add l10n

--- lib/features/notes/presentation/screens/new_note_screen.dart
+++ lib/features/notes/presentation/screens/new_note_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/core/extensions/build_context_ext.dart';
 
 @RoutePage()
 class NewNoteScreen extends StatelessWidget {
@@ -10,7 +11,7 @@ class NewNoteScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Add Note'),
+        title: Text(context.appLocalizations.addNote),
       ),
       body: const Center(
         child: Text('note TextForm'),
--- lib/features/notes/presentation/screens/notes_screen.dart
+++ lib/features/notes/presentation/screens/notes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notes/core/extensions/build_context_ext.dart';
 import 'package:flutter_notes/core/injection/injectable.dart';
 import 'package:flutter_notes/core/routers/app_router.dart';
 
@@ -12,7 +13,7 @@ class NotesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Notes'),
+        title: Text(context.appLocalizations.notes),
       ),
       body: const Center(
         child: Text('notes'),
@@ -21,7 +22,7 @@ class NotesScreen extends StatelessWidget {
         onPressed: () {
           getIt<AppRouter>().push(const NewNoteRoute());
         },
-        tooltip: 'Add new note',
+        tooltip: context.appLocalizations.addNote,
         child: const Icon(Icons.add),
       ),
     );
